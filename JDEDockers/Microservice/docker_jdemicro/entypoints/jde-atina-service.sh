95597ec440226831235a41762ca8131f061b8814
Javier <javier.godino@acqua.net.ar>
2019-10-24 19:11:56 -0300

    Update

--- JDEDockers/Microservice/docker_jdemicro/entypoints/jde-atina-service.sh
+++ JDEDockers/Microservice/docker_jdemicro/entypoints/jde-atina-service.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo '----------------------------------------------------' >>start.log
-echo 'Starting JDE Microservice' >>start.log
-echo '----------------------------------------------------' >>start.log
-echo 'CODE: ' >>start.log
-echo $JDE_MICROSERVER_CODE >>start.log
-echo '----------------------------------------------------' >>start.log
-echo "Service Name: " ${JDE_MICROSERVER_IP} >>start.log
-echo "Service Port: " ${JDE_MICROSERVER_PORT} >>start.log
-echo '----------------------------------------------------' >>start.log
+echo '----------------------------------------------------' >/tmp/start.log
+echo 'Starting JDE Microservice' >>/tmp/start.log
+echo '----------------------------------------------------' >>/tmp/start.log
+echo 'CODE: ' >>/tmp/start.log
+echo $JDE_MICROSERVER_CODE >>/tmp/start.log
+echo '----------------------------------------------------' >>/tmp/start.log
+echo "Service Name: " ${JDE_MICROSERVER_IP} >>/tmp/start.log
+echo "Service Port: " ${JDE_MICROSERVER_PORT} >>/tmp/start.log
+echo '----------------------------------------------------' >>/tmp/start.log
 cd  /var/jdeatinaserver
 ${JAVA_HOME}/bin/java -Xmx2G -jar /var/jdeatinaserver/JDEAtinaServer-1.0.0.jar -ipServicio $JDE_MICROSERVER_IP -portServicio $JDE_MICROSERVER_PORT -localIP 0.0.0.0 -clientcod $JDE_MICROSERVER_CODE
 
