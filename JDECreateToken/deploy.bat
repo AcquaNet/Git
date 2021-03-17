92aa6179f36de4eca624ff657cd5d08c932f71c2
AcquaNet <javier.godino@acqua.net.ar>
2021-03-17 17:04:24 -0300

    Agregado de versiones en el armado del microservicio y descarga de version configurable.

--- JDECreateToken/deploy.bat
+++ JDECreateToken/deploy.bat
@@ -1,12 +1,14 @@
 @echo off
 echo --------------------------------------------
-echo Building JDECreateToken...
+echo Deploying JDECreateToken...
 echo --------------------------------------------
-CALL mvn clean package
+CALL mvn clean package -P AcquaIT
 echo --------------------------------------------
 echo Copying Jar JDECreateToken
 echo --------------------------------------------
 COPY /Y target\JDECreateToken-1.0.0-jar-with-dependencies.jar C:\_work\JDEConnectorCE\Delivery\Docker\tools\JDECreateToken-1.0.0.jar
+COPY /Y target\JDECreateToken-1.0.0-jar-with-dependencies.jar target\JDECreateToken-1.0.0.jar
+mvn deploy:deploy-file -DgroupId=com.atina -DartifactId=pdf-process-addon-jde-layer -Dversion=1.0.0 -DrepositoryId=acquanet-central -Dpackaging=jar -Dfile=target\pdf-process-addon-jde-layer-1.0.0.jar -Durl=http://157.245.236.175:8081/artifactory/libs-release
 ECHO ==========================================================
 ECHO Press Enter to end
 ECHO ==========================================================
--- JDEDockers/Microservice/docker_jdemicroserver/entypoints/jde-atina-service.sh
+++ JDEDockers/Microservice/docker_jdemicroserver/entypoints/jde-atina-service.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
-echo '----------------------------------------------------' >/tmp/start.log
-echo 'Updating Librarie StdWebService' >>/tmp/start.log
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=$REPOSITORY_PROTOCOL://$REPOSITORY_URL -Dartifact=com.jdedwards:StdWebService:1.0.0 -Ddest=/tmp/jde/StdWebService-1.0.0.jar >>/tmp/start.log
-echo 'Updating Librarie jde-lib-wrapped' >>/tmp/start.log
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=$REPOSITORY_PROTOCOL://$REPOSITORY_URL -Dartifact=com.jdedwards:jde-lib-wrapped:1.0.0 -Ddest=/tmp/jde/jde-lib-wrapped-1.0.0.jar	>>/tmp/start.log
-echo 'Updating Microservice' >>/tmp/start.log
-mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=$REPOSITORY_PROTOCOL://$REPOSITORY_URL -Dartifact=com.atina:JDEAtinaServer:1.0.0 -Ddest=/var/jdeatinaserver/JDEAtinaServer-1.0.0.jar	>>/tmp/start.log
+echo '-SERVICE--------------------------------------------' >/tmp/start.log
+echo "   Name: " ${JDE_MICROSERVER_IP} >>/tmp/start.log
+echo "   Port: " ${JDE_MICROSERVER_PORT} >>/tmp/start.log
+echo '-REPOSITORY-----------------------------------------' >>/tmp/start.log
+echo "   Customer: " ${CUSTOMER_REPOSITORY_PROTOCOL}:${CUSTOMER_REPOSITORY_URL} >>/tmp/start.log
+echo "   Atina:    " ${ATINA_REPOSITORY_PROTOCOL}:${ATINA_REPOSITORY_URL} >>/tmp/start.log
+echo '-LIBRARIES------------------------------------------' >>/tmp/start.log
+echo '   Updating Librarie StdWebService Version ' ${JDE_MICROSERVER_STD_WEB_SERVICE} >>/tmp/start.log
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=$CUSTOMER_REPOSITORY_PROTOCOL://$CUSTOMER_REPOSITORY_URL -Dartifact=com.jdedwards:StdWebService:$JDE_MICROSERVER_STD_WEB_SERVICE -Ddest=/tmp/jde/StdWebService-$JDE_MICROSERVER_STD_WEB_SERVICE.jar >>/tmp/start.log
+echo '   Updating Librarie jde-lib-wrapped ' ${JDE_MICROSERVER_JDE_LIB_WRAPPED} >>/tmp/start.log
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=$CUSTOMER_REPOSITORY_PROTOCOL://$CUSTOMER_REPOSITORY_URL -Dartifact=com.jdedwards:jde-lib-wrapped:$JDE_MICROSERVER_JDE_LIB_WRAPPED -Ddest=/tmp/jde/jde-lib-wrapped-$JDE_MICROSERVER_JDE_LIB_WRAPPED.jar >>/tmp/start.log
+echo '   Updating JDEAtinaServer ' ${JDE_MICROSERVER_JDE_ATINA_SERVER}>>/tmp/start.log
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=$ATINA_REPOSITORY_PROTOCOL://$ATINA_REPOSITORY_URL -Dartifact=com.atina:JDEAtinaServer:$JDE_MICROSERVER_JDE_ATINA_SERVER -Ddest=/var/jdeatinaserver/JDEAtinaServer-$JDE_MICROSERVER_JDE_ATINA_SERVER.jar >>/tmp/start.log
+echo '-LICENSE--------------------------------------------' >>/tmp/start.log 
+echo '   Code: ' $JDE_MICROSERVER_CODE >>/tmp/start.log
+echo '-MICROSERVER----------------------------------------' >>/tmp/start.log
+echo "   JDE_MICROSERVER_SECRET_KEY: " ${JDE_MICROSERVER_SECRET_KEY} >>/tmp/start.log
+echo "   JDE_MICROSERVER_TOKEN_EXPIRATION: " ${JDE_MICROSERVER_TOKEN_EXPIRATION} >>/tmp/start.log
+echo "   JDE_MICROSERVER_ENTERPRISE_SERVER_NAME: " ${JDE_MICROSERVER_ENTERPRISE_SERVER_NAME} >>/tmp/start.log
+echo "   JDE_MICROSERVER_ENTERPRISE_SERVER_IP: " ${JDE_MICROSERVER_ENTERPRISE_SERVER_IP} >>/tmp/start.log
+echo "   JDE_MICROSERVER_ENTERPRISE_DB_NAME: " ${JDE_MICROSERVER_ENTERPRISE_DB_NAME} >>/tmp/start.log
+echo "   JDE_MICROSERVER_ENTERPRISE_DB_IP: " ${JDE_MICROSERVER_ENTERPRISE_DB_IP} >>/tmp/start.log
 echo '----------------------------------------------------' >>/tmp/start.log
-echo '----------------------------------------------------' >>/tmp/start.log
-echo 'Starting JDE Microservice' >>/tmp/start.log
-echo '----------------------------------------------------' >>/tmp/start.log
-echo 'CODE: ' >>/tmp/start.log
-echo $JDE_MICROSERVER_CODE >>/tmp/start.log
-echo '----------------------------------------------------' >>/tmp/start.log
-echo "Service Name: " ${JDE_MICROSERVER_IP} >>/tmp/start.log
-echo "Service Port: " ${JDE_MICROSERVER_PORT} >>/tmp/start.log
-echo "JDE_LIB_WRAPPED: " ${JDE_MICROSERVER_JDE_LIB_WRAPPED} >>/tmp/start.log
-echo "STD_WEB_SERVICE: " ${JDE_MICROSERVER_STD_WEB_SERVICE} >>/tmp/start.log
-echo "JDE_MICROSERVER_SECRET_KEY: " ${JDE_MICROSERVER_SECRET_KEY} >>/tmp/start.log
-echo "JDE_MICROSERVER_TOKEN_EXPIRATION: " ${JDE_MICROSERVER_TOKEN_EXPIRATION} >>/tmp/start.log
-echo "JDE_MICROSERVER_ENTERPRISE_SERVER_NAME: " ${JDE_MICROSERVER_ENTERPRISE_SERVER_NAME} >>/tmp/start.log
-echo "JDE_MICROSERVER_ENTERPRISE_SERVER_IP: " ${JDE_MICROSERVER_ENTERPRISE_SERVER_IP} >>/tmp/start.log
-echo "JDE_MICROSERVER_ENTERPRISE_DB_NAME: " ${JDE_MICROSERVER_ENTERPRISE_DB_NAME} >>/tmp/start.log
-echo "JDE_MICROSERVER_ENTERPRISE_DB_IP: " ${JDE_MICROSERVER_ENTERPRISE_DB_IP} >>/tmp/start.log
-echo '----------------------------------------------------' >>/tmp/start.log
-echo "ADDTIONAL SCRIPT: " >>/tmp/start.log
+echo "ADDITIONAL SCRIPT: " >>/tmp/start.log
 echo ${JDE_MICROSERVER_ENTERPRISE_SERVER_IP} ${JDE_MICROSERVER_ENTERPRISE_SERVER_NAME} >> /etc/hosts
 echo ${JDE_MICROSERVER_ENTERPRISE_DB_IP} ${JDE_MICROSERVER_ENTERPRISE_DB_NAME} >> /etc/hosts 
 cat /etc/hosts >>/tmp/start.log
 echo '----------------------------------------------------' >>/tmp/start.log
 cd  /var/jdeatinaserver
-${JAVA_HOME}/bin/java -Xmx2G -Djava.io.tmpdir=/tmp/jde -jar /var/jdeatinaserver/JDEAtinaServer-1.0.0.jar -ipServicio $JDE_MICROSERVER_IP -portServicio $JDE_MICROSERVER_PORT -localIP 0.0.0.0 -clientcod $JDE_MICROSERVER_CODE  -jdeLibWrappedVersion $JDE_MICROSERVER_JDE_LIB_WRAPPED -StdWebServiceVersion $JDE_MICROSERVER_STD_WEB_SERVICE  -secretKey $JDE_MICROSERVER_SECRET_KEY -tokenExpiration $JDE_MICROSERVER_TOKEN_EXPIRATION     
+${JAVA_HOME}/bin/java -Xmx2G -Djava.io.tmpdir=/tmp/jde -jar /var/jdeatinaserver/JDEAtinaServer-$JDE_MICROSERVER_STD_WEB_SERVICE.jar -ipServicio $JDE_MICROSERVER_IP -portServicio $JDE_MICROSERVER_PORT -localIP 0.0.0.0 -clientcod $JDE_MICROSERVER_CODE  -jdeLibWrappedVersion $JDE_MICROSERVER_JDE_LIB_WRAPPED -StdWebServiceVersion $JDE_MICROSERVER_STD_WEB_SERVICE  -secretKey $JDE_MICROSERVER_SECRET_KEY -tokenExpiration $JDE_MICROSERVER_TOKEN_EXPIRATION     
 
