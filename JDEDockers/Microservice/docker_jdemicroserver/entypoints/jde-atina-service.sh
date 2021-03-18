eb9a9e0f65354bc01469ce6720c33c7db28e2c26
AcquaNet <javier.godino@acqua.net.ar>
2021-03-18 16:57:37 -0300

    Modificacion del Docker JDE Microserver para permitir Mocking

--- JDEDockers/Microservice/docker_jdemicroserver/entypoints/jde-atina-service.sh
+++ JDEDockers/Microservice/docker_jdemicroserver/entypoints/jde-atina-service.sh
@@ -5,6 +5,13 @@ echo "   Port: " ${JDE_MICROSERVER_PORT} >>/tmp/start.log
 echo '-REPOSITORY-----------------------------------------' >>/tmp/start.log
 echo "   Customer: " ${CUSTOMER_REPOSITORY_PROTOCOL}:${CUSTOMER_REPOSITORY_URL} >>/tmp/start.log
 echo "   Atina:    " ${ATINA_REPOSITORY_PROTOCOL}:${ATINA_REPOSITORY_URL} >>/tmp/start.log
+echo '-MOCKING------------------------------------------' >>/tmp/start.log
+if [ "${JDE_MICROSERVER_MOCKING}" = "1" ]; then
+	cp -avr /tmp/jde/mocking/* /tmp/jde/
+	echo "   Mocking Activo. Metadata has been copied to /tmp/jde" >>/tmp/start.log
+else
+	echo "   Mocking Inactivo" >>/tmp/start.log
+fi;
 echo '-LIBRARIES------------------------------------------' >>/tmp/start.log
 echo '   Updating Librarie StdWebService Version ' ${STD_WEB_SERVICE_VERSION} >>/tmp/start.log
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:get -DremoteRepositories=$CUSTOMER_REPOSITORY_PROTOCOL://$CUSTOMER_REPOSITORY_URL -Dartifact=com.jdedwards:StdWebService:$STD_WEB_SERVICE_VERSION -Ddest=/tmp/jde/StdWebService-$STD_WEB_SERVICE_VERSION.jar >>/tmp/start.log
@@ -31,13 +38,15 @@ echo "   JDE_MICROSERVER_ENTERPRISE_SERVER_NAME: " ${JDE_MICROSERVER_ENTERPRISE_
 echo "   JDE_MICROSERVER_ENTERPRISE_SERVER_IP: " ${JDE_MICROSERVER_ENTERPRISE_SERVER_IP} >>/tmp/start.log
 echo "   JDE_MICROSERVER_ENTERPRISE_DB_NAME: " ${JDE_MICROSERVER_ENTERPRISE_DB_NAME} >>/tmp/start.log
 echo "   JDE_MICROSERVER_ENTERPRISE_DB_IP: " ${JDE_MICROSERVER_ENTERPRISE_DB_IP} >>/tmp/start.log
+echo "   JDE_MICROSERVER_MOCKING: " ${JDE_MICROSERVER_MOCKING} >>/tmp/start.log
 echo '----------------------------------------------------' >>/tmp/start.log
 echo "ADDITIONAL SCRIPT: " >>/tmp/start.log
 echo ${JDE_MICROSERVER_ENTERPRISE_SERVER_IP} ${JDE_MICROSERVER_ENTERPRISE_SERVER_NAME} >> /etc/hosts
 echo ${JDE_MICROSERVER_ENTERPRISE_DB_IP} ${JDE_MICROSERVER_ENTERPRISE_DB_NAME} >> /etc/hosts 
 cat /etc/hosts >>/tmp/start.log
 echo '----------------------------------------------------' >>/tmp/start.log
-echo ' Check log cat /tmp/jde/JDEConnectorServerLog/jde_atina_server_YYYY-MM-DD.0.log ' >>/tmp/start.log
+today_date=$(date --rfc-3339=date)
+echo ' Check log cat /tmp/jde/JDEConnectorServerLog/jde_atina_server_'$today_date'.0.log ' >>/tmp/start.log
 cd  /var/jdeatinaserver
 ${JAVA_HOME}/bin/java -Xmx2G -Djava.io.tmpdir=/tmp/jde -jar /var/jdeatinaserver/JDEAtinaServer-$STD_WEB_SERVICE_VERSION.jar -ipServicio $JDE_MICROSERVER_IP -portServicio $JDE_MICROSERVER_PORT -localIP 0.0.0.0 -clientcod $JDE_MICROSERVER_CODE  -jdeLibWrappedVersion $JDE_LIB_WRAPPED_VERSION -StdWebServiceVersion $STD_WEB_SERVICE_VERSION -JDEConnectorServerVersion $JDE_ATINA_SERVER_VERSION -secretKey $JDE_MICROSERVER_SECRET_KEY -tokenExpiration $JDE_MICROSERVER_TOKEN_EXPIRATION     
 
