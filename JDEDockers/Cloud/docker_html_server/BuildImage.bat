fc6680dd6d6be143cb4cee34bb4251eebb19e6c1
AcquaNet <javier.godino@acqua.net.ar>
2021-11-19 19:13:55 -0300

    Eliminacion de Docker Cloud

--- JDEDockers/Cloud/docker_html_server/BuildImage.bat
+++
@@ -1,5 +0,0 @@
-docker stop html-server
-docker rm html-server
-docker rmi soporteacqua/html-server:1.0.0
-docker build -f Dockerfile -t html-server .
-pause
--- JDEDockers/Cloud/docker_mulesoft_ngrok/BuildImage.bat
+++
@@ -1,2 +0,0 @@
-docker build -t soporteacqua/ngrok-server:1.0.0 .
-pause
