7635145c65a2f53e26658e842d7d1f1bb93b29ad
AcquaNet <javier.godino@acqua.net.ar>
2021-11-19 19:15:29 -0300

    Arreglos

--- 02-BuildJDEMicroserverDockerImage.bat
+++ 02-BuildJDEMicroserverDockerImage.bat
@@ -4,23 +4,27 @@ ECHO "Creating JDE Microservice Docker
 ECHO ==========================================================
 cd C:\_work\JDEConnectorCE\Projects\JDEDockers\Microservice
 ECHO Removing curren jdeatina-server...
-docker stop jdeatina-microserver
-docker rm jdeatina-microserver
-docker rmi 92455890/jdeatina-microserver:1.0.0
+docker stop jd-atina-microserver
+docker rm jd-atina-microserver
+docker rmi 92455890/jd-atina-microserver:1.0.0
 ECHO Start Building
 docker-compose up --no-start
+ECHO ==========================================================
+ECHO Docker Files
+ECHO ==========================================================
+"C:\Program Files\7-Zip\7z.exe" a -tzip jd-docker-files.zip .env docker-compose-dist.yml
+mvn deploy:deploy-file -DgroupId=com.atina -DartifactId=jd-docker-files -Dversion=1.0.0 -DrepositoryId=acquanet-central -Dpackaging=zip -Dfile=jd-docker-files.zip -Durl=http://157.245.236.175:8081/artifactory/libs-release 
+PAUSE >nul
 ECHO ==========================================================
 ECHO Press Enter to continue
 ECHO ==========================================================
 PAUSE >nul
 ECHO Moving image to Docker Hub
-docker login --username 92455890 --password Anita22223636
-docker push 92455890/jdeatina-microserver:1.0.0
-docker push 92455890/ngrok-microserver:1.0.0
+docker login -u 92455890 -p 97bcd883-fde2-46c6-9135-abda7d2c3a56
+docker push 92455890/jd-atina-microserver:1.0.0
 ECHO Starting Server...
-cd C:\_work\JDEConnectorCE\Projects\JDEDockers\Microservice
-docker cp config/JPS920 jdeatina-microserver:/tmp/jde/config
-docker cp /tmp/build_jde_libs/jde-lib-wrapped-1.0.0.jar jdeatina-microserver:/tmp/jde
-docker cp /tmp/build_jde_libs/StdWebService-1.0.0.jar jdeatina-microserver:/tmp/jde
+docker cp /tmp/build_jde_libs/JPS920 jd-atina-microserver:/tmp/jde/config
+docker cp /tmp/build_jde_libs/jde-lib-wrapped-1.0.0.jar jd-atina-microserver:/tmp/jde
+docker cp /tmp/build_jde_libs/StdWebService-1.0.0.jar jd-atina-microserver:/tmp/jde
 docker-compose start
 pause
--- JDEConnectorServer/run.bat
+++ JDEConnectorServer/run.bat
@@ -16,4 +16,4 @@ xcopy requirenment c:\tmp /E /C /I /Q /G /H /R /K /Y /Z /J
 ECHO ==========================================================
 ECHO "Starting Connector Server...
 ECHO ==========================================================
-java -Djava.io.tmpdir=/tmp/jde -jar target/JDEConnectorServer-jar-with-dependencies.jar -ipServicio localhost -portServicio 8085 -localIP 0.0.0.0 -clientcod demo -jdeLibWrappedVersion 1.0.0 -StdWebServiceVersion 1.0.0 -secretKey 123456789012345678901234567890123456789012345678901234567890 -tokenExpiration 480000
+java -Djava.io.tmpdir=/tmp/jde -jar target/JDEConnectorServer-jar-with-dependencies.jar -ipServicio localhost -portServicio 8085 -localIP 0.0.0.0 -clientcod demo -jdeLibWrappedVersion 1.0.0 -StdWebServiceVersion 1.0.0 -JDEConnectorServerVersion 1.0.0 -secretKey 123456789012345678901234567890123456789012345678901234567890 -tokenExpiration 4800000 -JDEConnectorServerMocking 0
