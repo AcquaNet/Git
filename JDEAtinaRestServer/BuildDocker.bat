2487b2876d463598a1b3bb7e626a2aa3ae551b5a
AcquaNet <javier.godino@acqua.net.ar>
2021-12-07 18:11:38 -0300

    Update

--- JDEAtinaRestServer/BuildDocker.bat
+++ JDEAtinaRestServer/BuildDocker.bat
@@ -8,12 +8,19 @@ set JAVA_HOME=C:\Program Files\Java\openjdk-11
 set Path=%JAVA_HOME%\bin;%Path%
 echo Java 11 activated.
 # 
+#
+CALL docker build -t 92455890/jd-atina-rest-server:1.0.0 .
+docker run -p 443:443 92455890/jd-atina-rest-server
+#
+#
 ECHO **************************************************************************************
 ECHO Building Console Docker
 ECHO **************************************************************************************
 # 
 #
 CALL mvnw package
+CALL docker build -f src/main/docker/Dockerfile.jvm -t 92455890/jd-atina-rest-server .
+CALL docker run -i --rm -p 443:443 92455890/jd-atina-rest-server
 #
 ECHO **************************************************************************************
 ECHO Running Locally
@@ -21,8 +28,15 @@ ECHO ***************************************************************************
 #
 CALL java -jar target/quarkus-app/quarkus-run.jar
 #
-
+ECHO **************************************************************************************
+ECHO Create Image Native
+ECHO **************************************************************************************
 #
+CALL mvnw package -Pnative -Dquarkus.native.remote-container-build=true -Dquarkus.container-image.build=true
+#
+CALL mvnw clean package -Dquarkus.container-image.push=true
+#
+CALL docker run -i --rm -p 443:443 registry-1.docker.io/92455890/jd-atina-rest-server:1.0.0
 #
 
 
