931bac0d9664a0f0725609b9c950c434bddd6e5f
AcquaNet <javier.godino@acqua.net.ar>
2021-12-20 16:10:59 -0300

    Actualizacion del generador de WS para llamar a BSFN

--- JDEGenerateWS/src/main/java/com/atina/ppal/Main.java
+++ JDEGenerateWS/src/main/java/com/atina/ppal/Main.java
@@ -5,11 +5,15 @@
  */
 package com.atina.ppal;
   
+import com.acqua.atina.jdeconnector.internal.model.JDEBsfnParameter;
+import com.acqua.atina.jdeconnectorservice.service.JDESingleConnection;
 import com.acqua.atina.jdeconnectorservice.service.poolconnection.JDEPoolConnections;
 import com.acqua.atina.jdeconnectorservice.wsservice.JDESingleWSConnection;
 import com.acqua.jde.jdeconnectorserver.model.Configuracion;
 import com.atina.buildjdebundle.exceptions.MetadataServerException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
 import oracle.e1.bssv.JP010000.AddressBookManager;
 import oracle.e1.bssv.JP010000.valueobject.GetAddressBook;
 import oracle.e1.bssv.JP010000.valueobject.ShowAddressBook;
@@ -41,6 +45,86 @@ public class Main {
      */
     public static void main(String[] args) throws MetadataServerException, IOException, BusinessServiceException   {
        
+        
+        //createZFile(args);
+        
+        callBSFN(args);
+        
+    } 
+    
+    private static void callBSFN(String[] args) throws BusinessServiceException
+    {
+        
+        Configuracion config = new Configuracion();
+        
+        config.setUser("JDE");
+
+        config.setPassword("Modus2020!");
+
+        config.setEnvironment("JPS920");
+
+        config.setRole("*ALL");
+
+        config.setSessionId(0);
+        
+        config.setTokenExpiration(2400000);
+        
+        config.setMocking(0);
+        
+        int sessionID = JDEPoolConnections.getInstance().createConnection(  config.getUser(), 
+                                                                                config.getPassword(), 
+                                                                                config.getEnvironment(), 
+                                                                                config.getRole(), 
+                                                                                config.getSessionIdAsInt(),
+                                                                                false);
+        config.setSessionId(sessionID);
+         
+        logger.info("              Request Received: " + config.toString());
+        
+      
+        // ==============================================================
+        // Get Connection
+        // ==============================================================
+        
+        JDESingleConnection singleConnection = (JDESingleConnection) JDEPoolConnections.getInstance().getSingleConnection(sessionID);
+        
+         // ==============================================================
+         // Get BSFN List
+         // ==============================================================
+         
+        Set<String> operationList = singleConnection.getOperationList();
+        
+         // ==============================================================
+         // Get BSFN Parameters
+         // ==============================================================
+         
+        Set<JDEBsfnParameter> parameters = singleConnection.getBSFNParameter("AddressBookMasterMBF");
+         
+        
+        // ==============================================================
+        // Call BSFN
+        // ==============================================================
+         
+        //cActionCode: 'I',
+	//: 1
+                
+        HashMap<String,Object> inputValues = new HashMap<String,Object>();
+        inputValues.put("cActionCode", "I");
+        inputValues.put("mnAddressBookNumber", 1); 
+        
+         HashMap<String, Object> reponse = singleConnection.callJDEOperation("AddressBookMasterMBF", inputValues, 0);
+        
+        // ==============================================================
+        // Logout
+        // ==============================================================
+        
+        JDEPoolConnections.getInstance().disconnect(config.getSessionId());
+        
+        
+    }
+    
+    private static void createZFile(String[] args) throws BusinessServiceException
+    {
         Configuracion config = new Configuracion();
         
         config.setUser("JDE");
@@ -86,38 +170,7 @@ public class Main {
         // ==============================================================
  
         E1Principal e1ppal = singleConnection.getClient().getE1ppal();
-         
-        
-        // ==============================================================
-         // Set Context
-         // ==============================================================
-         
-//        IContext context = new oracle.e1.bssvfoundation.impl.base.Context(true,e1ppal,"JP010000", "getAddressBook"); 
-//        
-//        context.setApplicationID("JP010000");
-//        context.setGUID("AddressBookManager");
-//        context.setWorkstationName("WS");
-//        
-//        ((Context) context).incrementPublishedMethodCounter();
-        
-         // ==============================================================
-         // Call Operation
-         // ==============================================================
-         // String operation = "oracle.e1.bssv.JP590000.FinancialsManager.getGLAccount";
-         // oracle.e1.bssv.JP010000.AddressBookManager.getAddressBook
-         
-//        AddressBookManager objectToCall = new oracle.e1.bssv.JP010000.AddressBookManager();
-//        
-//        GetAddressBook account = new oracle.e1.bssv.JP010000.valueobject.GetAddressBook();
-//        
-//        Entity entity = new Entity();
-//        entity.setEntityId(28);
-//        
-//        account.setEntity(entity);
-//         
-//        ShowAddressBook returnValue = objectToCall.getAddressBook(context, null, account);
-//        
-        
+          
         // ==============================================================
          // Set Context
          // ==============================================================
@@ -129,6 +182,7 @@ public class Main {
         context.setWorkstationName("WS");
         
         ((Context) context).incrementPublishedMethodCounter();
+        
         
          // ==============================================================
          // Call Operation
@@ -152,20 +206,15 @@ public class Main {
          
                 
          ConfirmInsertBatchJournalEntryStaging returnValue = objectToCall.insertBatchJournalEntry(context, null, vo);
-        
-        
-        
-        
+         
         
         // ==============================================================
         // Logout
         // ==============================================================
         
         JDEPoolConnections.getInstance().disconnect(config.getSessionId());
-        
-        
-        
-    } 
+         
+    }
      
       
      
